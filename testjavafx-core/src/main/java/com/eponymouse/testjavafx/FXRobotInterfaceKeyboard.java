e3fed111ae85c3118cc09c835ad69bbb55ba3db3
Neil Brown <>
2022-10-25 18:18:16 +0100

    Added documentation to some more classes and methods.

--- testjavafx-core/src/main/java/com/eponymouse/testjavafx/FXRobotInterfaceKeyboard.java
+++ testjavafx-core/src/main/java/com/eponymouse/testjavafx/FXRobotInterfaceKeyboard.java
@@ -15,37 +15,73 @@ package com.eponymouse.testjavafx;
 
 import javafx.scene.input.KeyCode;
 
+/**
+ * A set of methods for testing keyboard interactions.
+ * All the methods return this for easy chaining.
+ * 
+ * Calling these methods from the FX thread is safe
+ * but the events will not have been processed on return
+ * from the methods.  The intended use is calling from another
+ * thread, where the methods should have been processed by
+ * the time the method returns.  See {@link FxThreadUtils#waitForFxEvents()}
+ * for more information.
+ */
 public interface FXRobotInterfaceKeyboard<T extends FXRobotInterfaceKeyboard<T>>
 {
     /**
      * Presses all the keys in order, then releases
-     * them all in reverse order, then waits for
-     * the FX events thread.
+     * them all in reverse order, then calls
+     * {@link FxThreadUtils#waitForFxEvents()}.
      */
     public T push(KeyCode... keyCodes);
 
     /**
-     * Presses all the keys in the given order, then waits for
-     * the FX events thread.
+     * Presses all the keys in the given order, then calls
+     * {@link FxThreadUtils#waitForFxEvents()}.
+     * 
+     * Note that depending on the operating system, keys
+     * pressed may generate repeated key typed events until
+     * they are released.
      */
     public T press(KeyCode... keyCodes);
 
     /**
-     * Releases all the keys in the given order, then waits for
-     * the FX events thread.  Calling with no arguments releases
+     * Releases all the keys in the given order, then calls
+     * {@link FxThreadUtils#waitForFxEvents()}.  Calling with no arguments releases
      * all keys held down by previously calling press.
      */
     public T release(KeyCode... keyCodes);
 
+    /**
+     * Writes the given character using a key press/typed/released
+     * event sequence, then calls {@link FxThreadUtils#waitForFxEvents()}.
+     * @param c The character to write
+     */
     public default T write(char c)
     {
         return write(Character.toString(c));
     }
 
+    /**
+     * Writes the given String, one character at a time,
+     * using a key press/typed/released event sequence,
+     * then calls {@link FxThreadUtils#waitForFxEvents()}.
+     * @param text The text to write.
+     */
     public default T write(String text)
     {
         return write(text, 0);
     }
 
+    /**
+     * Writes the given String, one character at a time,
+     * using a key press/typed/released event sequence,
+     * then calls {@link FxThreadUtils#waitForFxEvents()}.
+     * It sleeps for the given number of milliseconds
+     * between each character.
+     * 
+     * @param text The text to write.
+     * @param millisecondDelay The number of milliseconds to wait between each character.
+     */
     public T write(String text, int millisecondDelay);
 }
--- testjavafx-core/src/main/java/com/eponymouse/testjavafx/FxRobotInterface.java
+++ testjavafx-core/src/main/java/com/eponymouse/testjavafx/FxRobotInterface.java
@@ -21,20 +21,71 @@ import javafx.stage.Window;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * The main interface with all the methods available to
+ * call to fake GUI events.
+ * 
+ * See the subclasses {@link FXRobotInterfaceKeyboard} and
+ * {@link FxRobotInterfaceMouse} for documentation on keyboard
+ * and mouse events respectively.
+ * 
+ * Any method that returns FxRobotInterface returns this
+ * for easy chaining.
+ */
 public interface FxRobotInterface extends FXRobotInterfaceKeyboard<FxRobotInterface>, FxRobotInterfaceMouse<FxRobotInterface>
 {
+    /**
+     * Sleep for the given number of milliseconds.
+     * 
+     * This method is safe to call on the FX thread,
+     * although you probably don't want to block that
+     * thread.
+     */
     FxRobotInterface sleep(int milliseconds);
-    
+
+    /**
+     * Gets a list of all currently showing JavaFX windows.
+     * 
+     * This method is safe to call on the FX thread.  If
+     * called on another thread it waits to fetch the list
+     * from the FX thread, and will block if the FX thread
+     * is busy.
+     */
     public List<Window> listWindows();
-    
+
     public Window targetWindow();
 
     public NodeQuery lookup(String query);
 
     public NodeQuery lookup(Predicate<Node> nodePredicate);
 
+    /**
+     * Starts a NodeQuery search with the given nodes as an initial result set.
+     * 
+     * So calling from(myNodes).queryAll() is equivalent to calling
+     * Set.of(myNodes).  But calling from(myNodes).lookup(".wide").queryAll() will find
+     * all the nodes anywhere within myNodes with the style-class wide.
+     * 
+     * This method can be called from any thread.
+     * 
+     * @param useAsRoots The node or nodes to use as the start of the NodeQuery search.
+     *                   Calling it with an empty list will give a NodeQuery with no results. 
+     * @return The NodeQuery with the given nodes as the initial results.
+     */
     public NodeQuery from(Node... useAsRoots);
 
+    /**
+     * Gets the centre of the given Node's bounds as
+     * a coordinate on the screen.
+     * 
+     * This method is safe to call on the FX thread.  If
+     * called on another thread it waits to access the bounds
+     * on the FX thread, and will block if the FX thread
+     * is busy.
+     * 
+     * @param node The node to fetch the centre coordinates for.
+     * @return The centre of the node as screen coordinates.
+     */
     public Point2D point(Node node);
 
     public default Point2D point(String query)
