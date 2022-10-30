632921b87953650d9e9f627c66f39ee6ada5c8dc
Neil Brown <github@twistedsquare.com>
2022-10-30 17:10:56 +0000

    Documented FxRobotInterfaceWindow.

--- testjavafx-core/src/main/java/org/testjavafx/FxRobotInterfaceWindow.java
+++ testjavafx-core/src/main/java/org/testjavafx/FxRobotInterfaceWindow.java
@@ -17,25 +17,65 @@ import com.google.common.collect.ImmutableList;
 import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.testjavafx.node.NodeQuery;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * A set of methods for accessing windows.
+ * All the methods return this for easy chaining.
+ *
+ * <p>Calling these methods from the FX thread is safe.
+ *
+ * <p>Note that currently, the window methods are slightly different than
+ * TestFX.  TestFX allows explicit setting and querying of the target window,
+ * which can be confusing because it conflates the idea of the focused window
+ * with the idea of setting a specific target window.
+ * In TestJavaFX if you want the focused window, use {@link #focusedWindow()}
+ * instead of the targetWindow() call to get the currently focused window.
+ * If you want to target a specific window, use the {@link #from(Window)} call
+ * to begin the query.
+ *
+ * @param <T> The type of this object to be returned from all the methods.
+ *            This will be {@link FxRobotInterface} if you use these methods via
+ *            {@link FxRobotInterface} or {@link FxRobot}.
+ */
 public interface FxRobotInterfaceWindow<T extends FxRobotInterfaceWindow<T>>
 {
+    /**
+     * Start a {@link NodeQuery} that will only look within the given window.
+     *
+     * @param window The window to restrict the NodeQuery to.
+     * @return A new NodeQuery restricted to the given window.
+     */
+    public NodeQuery from(Window window);
+
+    /**
+     * List all the potential target windows, ordered by the most likely
+     * window first.  Most likely here means that it is most closely related
+     * to the currently focused window, within the window parent/child hierarchy.
+     *
+     * <p>This method is safe to call on the FX thread.  If called off the FX thread
+     * it will block until it can perform the query on the FX thread.
+     *
+     * @return A list of all available windows, sorted by the closeness to
+     *         {@link #focusedWindow()} within the window hierarchy.
+     */
     public default List<Window> listTargetWindows()
     {
-        return fetchWindowsByProximityTo(focusedWindow());
-    }
-    
-    private List<Window> fetchWindowsByProximityTo(Window targetWindow)
-    {
-        List<Window> windows = listWindows();
-        return orderWindowsByProximityTo(targetWindow, windows);
+        return FxThreadUtils.syncFx(() -> fetchWindowsByProximityTo(focusedWindow()));
     }
 
+    // Only call on FX thread
+    private List<Window> fetchWindowsByProximityTo(Window targetWindow)
+    {
+        return orderWindowsByProximityTo(targetWindow, listWindows());
+    }
+
+    // Only call on FX thread
     private List<Window> orderWindowsByProximityTo(Window targetWindow, List<Window> windows)
     {
         List<Window> copy = new ArrayList<>(windows);
@@ -43,6 +83,7 @@ public interface FxRobotInterfaceWindow<T extends FxRobotInterfaceWindow<T>>
         return Collections.unmodifiableList(copy);
     }
 
+    // Only call on FX thread
     private int calculateWindowProximityTo(Window targetWindow, Window window)
     {
         if (window == targetWindow)
@@ -56,6 +97,7 @@ public interface FxRobotInterfaceWindow<T extends FxRobotInterfaceWindow<T>>
         return 2;
     }
 
+    // Only call on FX thread
     private boolean isOwnerOf(Window window, Window targetWindow)
     {
         Window ownerWindow = retrieveOwnerOf(window);
@@ -66,6 +108,7 @@ public interface FxRobotInterfaceWindow<T extends FxRobotInterfaceWindow<T>>
         return ownerWindow != null && isOwnerOf(ownerWindow, targetWindow);
     }
 
+    // Only call on FX thread
     private Window retrieveOwnerOf(Window window)
     {
         if (window instanceof Stage)
@@ -81,16 +124,40 @@ public interface FxRobotInterfaceWindow<T extends FxRobotInterfaceWindow<T>>
 
     /**
      * Gets a list of all currently showing JavaFX windows.
-     * 
-     * This method is safe to call on the FX thread.  If
+     *
+     * <p>This is essentially a thread-safe way to call {@link Window#getWindows()}.
+     *
+     * <p>This method is safe to call on the FX thread.  If
      * called on another thread it waits to fetch the list
      * from the FX thread, and will block if the FX thread
      * is busy.
+     *
+     * @return A list of all currently showing JavaFX windows
+     *         within the current application.
      */
     public default List<Window> listWindows()
     {
         return FxThreadUtils.syncFx(() -> ImmutableList.copyOf(Window.getWindows()));
     }
 
+    /**
+     * Get the currently focused window.
+     *
+     * <p>This method is safe to call on the FX thread.  If called on another
+     * thread it will block until it can its calculation on the FX thread.
+     *
+     * <p>This has a few extra complications.  I have seen cases on Monocle
+     * where two windows (a parent and child) both claim to be focused, so in that
+     * case we prefer the child.  Sometimes no window can be focused, but in this
+     * case we generally want to access the most recent return of focusedWindow(),
+     * so we return the value from the previous focusedWindow() call provided it is
+     * still showing.  If none of this applies and there is only one window available
+     * from {@link #listWindows()} we just return that as our best guess (but do not
+     * remember it for the previous-call mechanism just mentioned).  If all else fails
+     * we return null.
+     *
+     * @return Our best guess at the current/recently-focused window (see above) or
+     *         null if no such window is available.
+     */
     public Window focusedWindow();
 }
