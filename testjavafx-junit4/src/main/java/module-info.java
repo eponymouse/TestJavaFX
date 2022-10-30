21b3ed422eb59a50763d39662b681e74ace50b33
Neil Brown <github@twistedsquare.com>
2022-10-30 08:08:49 +0000

    Renamed module to keep javac happy.

--- testjavafx-junit4/src/main/java/module-info.java
+++ testjavafx-junit4/src/main/java/module-info.java
@@ -1,4 +1,5 @@
-module com.eponymouse.testjavafx.junit4 {
+// javac complains on modules named junit4, see https://bugs.openjdk.org/browse/JDK-8216185
+module com.eponymouse.testjavafx.junitFour {
     exports com.eponymouse.testjavafx.junit4;
     
     requires com.eponymouse.testjavafx.core;
