0bbb80d0d2e6657f71027113aaed355014017887
Neil Brown <github@twistedsquare.com>
2022-10-30 16:48:11 +0000

    Documented the Motion class.

--- testjavafx-core/src/main/java/org/testjavafx/Motion.java
+++ testjavafx-core/src/main/java/org/testjavafx/Motion.java
@@ -13,13 +13,42 @@
  */
 package org.testjavafx;
 
+/**
+ * Different types of mouse motion that can be used to move the mouse around.
+ */
 public enum Motion
 {
+    /**
+     * Instantly moves the mouse to the destination, without moving to any
+     * intermediate locations.  Fast and can be executed with less thread waiting,
+     * but unrealistic because it does not trigger any mouse entered/exited
+     * events or anything that activates on hover.
+     */
     TELEPORT,
+    /**
+     * Moves in a straight (potentially diagonal) line between the current position
+     * and the destination.  A realistic option but takes longer because it
+     * moves through intermediate locations at a realistic speed.
+     */
     STRAIGHT_LINE,
+    /**
+     * Move horizontally to the correct X location, then vertically to the correct
+     * Y location.
+     */
     HORIZONTAL_FIRST,
+    /**
+     * Move vertically to the correct Y location, then horizontally to the correct
+     * X location.  Particularly useful for activating menus and submenus, as it
+     * ensures the mouse doesn't leave the menu on its way between the parent
+     * and the child item.
+     */
     VERTICAL_FIRST;
 
+    /**
+     * The default motion.  Currently {@link #STRAIGHT_LINE}.
+     *
+     * @return The default mouse motion.
+     */
     @SuppressWarnings("checkstyle:MethodName")
     public static Motion DEFAULT()
     {
