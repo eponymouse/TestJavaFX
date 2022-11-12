84152a8b390d55429217e1090a8d7dfb9cb92133
Neil Brown <>
2022-11-12 17:23:19 +0000

    Added a popup window to the TAB test.

--- internal-test-junit4/src/test/java/TabTest.java
+++ internal-test-junit4/src/test/java/TabTest.java
@@ -12,10 +12,17 @@
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.PopupControl;
+import javafx.scene.control.Skin;
+import javafx.scene.control.Skinnable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
@@ -30,10 +37,10 @@ import org.testjavafx.junit4.ApplicationTest;
 
 public class TabTest extends ApplicationTest
 {
-
     private TextField field1;
     private TextField field2;
     private Modality modality = Modality.APPLICATION_MODAL;
+    private boolean showPopups = false;
     private TextField field3;
     private TextField field4;
 
@@ -61,12 +68,52 @@ public class TabTest extends ApplicationTest
                 field4
             ));
             d.getDialogPane().getButtonTypes().setAll(ButtonType.OK);
+            if (showPopups)
+            {
+                showPopupOnFocus(field3);
+                showPopupOnFocus(field4);
+            }
             d.showAndWait();
         });
         primaryStage.setScene(new Scene(new VBox(field1, field2, showDialog)));
         primaryStage.show();
     }
-    
+
+    private static void showPopupOnFocus(final TextField field)
+    {
+        field.focusedProperty().addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)
+            {
+                if (newValue)
+                {
+                    PopupControl popupControl = new PopupControl();
+                    popupControl.setSkin(new Skin<Skinnable>()
+                    {
+                        @Override
+                        public Skinnable getSkinnable()
+                        {
+                            return popupControl;
+                        }
+
+                        @Override
+                        public Node getNode()
+                        {
+                            return new Label("Label for " + field);
+                        }
+
+                        @Override
+                        public void dispose()
+                        {
+                        }
+                    });
+                    popupControl.show(field, 0, 0);
+                }
+            }
+        });
+    }
+
     @Test
     public void test()
     {
@@ -123,5 +170,26 @@ public class TabTest extends ApplicationTest
     {
         modality = Modality.NONE;
         testDialog();
+    }
+
+    @Test
+    public void testDialogWithPopup()
+    {
+        showPopups = true;
+        testDialog();
+    }
+
+    @Test
+    public void testDialogWindowModalWithPopup()
+    {
+        showPopups = true;
+        testDialogWindowModal();
+    }
+
+    @Test
+    public void testDialogNonModalWithPopup()
+    {
+        showPopups = true;
+        testDialogNonModal();
     }
 }
