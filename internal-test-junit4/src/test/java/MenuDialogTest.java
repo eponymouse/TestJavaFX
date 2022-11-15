f4b450a40e81ecead00621f3a1dac42e99d1179b
Neil Brown <>
2022-11-15 08:13:58 +0000

    Added a test to check focus in dialogs opened by menu (since menu is a popup window).

---
+++ internal-test-junit4/src/test/java/MenuDialogTest.java
@@ -0,0 +1,92 @@
+/*
+ * TestJavaFX: Testing for JavaFX applications
+ * Copyright (c) Neil Brown, 2022.
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.testjavafx.FxThreadUtils;
+import org.testjavafx.Motion;
+import org.testjavafx.junit4.ApplicationTest;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MenuDialogTest extends ApplicationTest
+{
+    private Stage parent;
+    private Button parentButton;
+    private Modality modality = Modality.APPLICATION_MODAL;
+    private AtomicReference<String> result = new AtomicReference<>("");
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        super.start(primaryStage);
+        parent = primaryStage;
+        parentButton = new Button();
+        MenuItem open = new MenuItem("Open");
+        open.setOnAction(e -> {
+            TextInputDialog textInputDialog = new TextInputDialog();
+            textInputDialog.initModality(modality);
+            textInputDialog.getEditor().getStyleClass().add("fake-file-chooser-dialog");
+            if (parent != null)
+                textInputDialog.initOwner(parent);
+            result.set(textInputDialog.showAndWait().orElse(null));
+        });
+        Menu menu = new Menu("Main", null, open);
+        BorderPane root = new BorderPane(parentButton);
+        root.setTop(new MenuBar(menu));
+        parent.setScene(new Scene(root));
+        parent.show();
+    }
+
+    @Test
+    public void test1()
+    {
+        MatcherAssert.assertThat(targetWindow(), Matchers.equalTo(FxThreadUtils.syncFx(() -> parentButton.getScene().getWindow())));
+        openMenu();
+        write("AB");
+        tap(KeyCode.ENTER);
+        MatcherAssert.assertThat(result.get(), Matchers.equalTo("AB"));
+        MatcherAssert.assertThat(targetWindow(), Matchers.equalTo(FxThreadUtils.syncFx(() -> parentButton.getScene().getWindow())));
+    }
+
+    private void openMenu()
+    {
+        clickOn("Main").moveTo("Open", Motion.VERTICAL_FIRST).clickOn();
+    }
+
+    @Test
+    public void test1b()
+    {
+        modality = Modality.WINDOW_MODAL;
+        test1();
+    }
+
+    @Test
+    public void test1c()
+    {
+        modality = Modality.NONE;
+        test1();
+    }
+}
